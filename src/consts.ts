5a266c28c6c5dd9112ab8d2090ac03a8b8d1f06a
lushisang <lushisang@gmail.com>
2025-04-22 13:31:57 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,6 @@ export const SITE_DESCRIPTION = "知行天人合一生命力";
 export const COLLECTION_MAP = {
   life: "😶‍🌫️猫生堡Life🏰",
   do: "🧑‍🌾猫干饭OBody🍚",
-  lab: "🧑‍💻猫工社LSystem🤖",
-  xyy: "🧚猫逍遥XSpirit🌼",
+  lab: "🧑‍💻猫工社LSys🤖",
+  xyy: "🧚猫逍遥XSpr🌼",
 };
