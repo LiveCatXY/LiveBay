2177c9f3903df996c3cbd0df2283becfef1c3467
lushisang <lushisang@gmail.com>
2025-07-19 09:53:58 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "🏡新自然逍遥湾🏝️";
-export const SITE_DESCRIPTION = "知行天人合一安护生命力";
+export const SITE_DESCRIPTION = "安护生命｜知行养生｜天人合一";
 export const COLLECTION_MAP = {
   life: "😶‍🌫️猫生堡🍑",
   xyy: "🧚猫逍遥🌼",
